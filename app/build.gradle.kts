4c2654d83138aec18cee31387dfb9d81b3588124
kei-1111 <kei_02_1111@icloud.com>
2024-12-04 17:35:30 +0900

    fix: Build出来ないバグの修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,12 +18,12 @@ plugins {
 
 android {
     namespace = "com.example.firebasesample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.firebasesample"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
